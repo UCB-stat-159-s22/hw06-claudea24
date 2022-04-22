d34e7c0b07a890804aa492afa2b34b96cce596b8
Claudea Jennefer <claudea@berkeley.edu>
2022-04-22 11:27:46 +0000

    try pytest, but does not work

---
+++ ligotools/tests/tests_readligo.py
@@ -0,0 +1,10 @@
+from ligotools import readligo as rl
+
+def test_read_frame():
+	pass
+
+def test_read_hdf5():
+	pass
+
+def test_loaddata():
+	pass
---
+++ ligotools/tests/tests_utils.py
@@ -0,0 +1,19 @@
+from ligotools.utils import readligo as rl
+
+def test_whiten():
+	pass
+
+def test_write_wavfile():
+	pass
+
+def test_reqshift():
+	pass
+
+def test_plot2():
+	pass
+
+def test_plot1():
+	pass
+
+def test_plot3():
+	pass
